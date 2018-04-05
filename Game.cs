cfc845dc9dad8acbc441d3492e5a70b6e326bee4
Christopher <christopher.d.donnelly@gmail.com>
2018-04-05 15:49:03 -0700

    Integrated card graphics into game and updated console logs.

--- Game.cs
+++ Game.cs
@@ -29,10 +29,25 @@ namespace Hackathon_Day
         }
         public int ChooseCards()
         {
+            
+            Console.ForegroundColor = ConsoleColor.Red;
             int player1Card = this.PlayCard(redPlayer);
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
             int player2Card = this.PlayCard(bluePlayer);
-            System.Console.WriteLine("\n Red Player played: " + player1Card);
-            System.Console.WriteLine("Blue Player played: " + player2Card + "\n");
+            // System.Console.WriteLine("\nRed Player played: " + player1Card);
+            // System.Console.WriteLine("Blue Player played: " + player2Card + "\n");
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red Player played: ");
+            Console.WriteLine(redPlayer.playedCards[redPlayer.playedCards.Count-1]);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Blue Player played: ");
+            Console.WriteLine(bluePlayer.playedCards[bluePlayer.playedCards.Count-1]);
+            
+            Console.ForegroundColor = ConsoleColor.White;
+
             int result = this.resultsTable[player1Card][player2Card];
             return result;
         }
@@ -40,9 +55,12 @@ namespace Hackathon_Day
         {
             Card tempCard = new Card();
             bool cardAvailable = false;
-            System.Console.WriteLine($"\n {colorPlayer} These are your cards: ");
-            foreach(Card c in colorPlayer.hand){System.Console.WriteLine(c.value + " - " + c.name + " - " + c.desc);}
-            System.Console.WriteLine($"\n {colorPlayer} What card will you play (enter card number)?");
+
+            Console.Clear();
+            Console.WriteLine($"{colorPlayer} These are your cards: ");
+            foreach(Card c in colorPlayer.hand){Console.WriteLine(c.value + " - " + c.name + " - " + c.desc);}
+
+            Console.Write($"\n{colorPlayer} What card will you play (enter card number)? ");
             int playerInput = int.Parse(Console.ReadLine());
             foreach(Card c in colorPlayer.hand)
             {
@@ -67,30 +85,40 @@ namespace Hackathon_Day
 
         public void DisplayRoundStats()
         {
-            System.Console.WriteLine("\n ===========================STATS=============================");
-            System.Console.WriteLine("Red player has played the following cards: ");
-            // foreach(Card c in redPlayer.playedCards){System.Console.WriteLine(c.value + " - " + c.name + " - " + c.desc);}
-            foreach(Card c in redPlayer.playedCards){System.Console.WriteLine(c.value + " - " + c.name);}
-            System.Console.WriteLine("Red player rounds won (current game): " + this.redPlayer.roundsWon);
-            System.Console.WriteLine("\n Blue player has played the following cards: ");
-            // foreach(Card c in bluePlayer.playedCards){System.Console.WriteLine(c.value + " - " + c.name + " - " + c.desc);}
-            foreach(Card c in bluePlayer.playedCards){System.Console.WriteLine(c.value + " - " + c.name);}
-            System.Console.WriteLine("Blue player rounds won (current game): " + this.bluePlayer.roundsWon);
-            System.Console.WriteLine("============================================================= \n");
+            Console.WriteLine("\n===========================STATS=============================");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red player has played the following cards: ");
+            foreach(Card c in redPlayer.playedCards){Console.WriteLine(c.value + " - " + c.name);}
+            Console.WriteLine("Red player rounds won (current game): " + this.redPlayer.roundsWon);
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nBlue player has played the following cards: ");
+            foreach(Card c in bluePlayer.playedCards){Console.WriteLine(c.value + " - " + c.name);}
+            Console.WriteLine("Blue player rounds won (current game): " + this.bluePlayer.roundsWon);
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("============================================================= \n");
+            Console.Write("Press Any Key to continue: " );
+            Console.ReadLine();
         }
         public void DisplayGameStats()
         {
-            System.Console.WriteLine("\n ===========================WINS=============================");
+            System.Console.WriteLine("\n===========================WINS=============================");
+            Console.ForegroundColor = ConsoleColor.Red;
             System.Console.WriteLine("Red player game wins: " + this.redPlayer.gamesWon);
+            Console.ForegroundColor = ConsoleColor.Blue;
             System.Console.WriteLine("Blue player game wins: " + this.bluePlayer.gamesWon);
+            Console.ForegroundColor = ConsoleColor.White;
             System.Console.WriteLine("============================================================= \n");
+            Console.Write("Press Any Key to continue: ");
+            Console.ReadLine();
         }
         public void CheckGameOver(Player colorPlayer,int result)
         {
             if(colorPlayer.roundsWon >= 4)
             {
                 colorPlayer.gamesWon += 1;
-                System.Console.WriteLine($"***** {colorPlayer} wins game! *****");
+                System.Console.WriteLine($"\n***** {colorPlayer} wins game! *****");
                 this.hold = 0;
                 this.DisplayGameStats();
                 colorPlayer.roundsWon = 0;
@@ -101,7 +129,9 @@ namespace Hackathon_Day
                 this.redPlayer.gamesWon += 1;
                 this.hold = 0;
                 this.DisplayGameStats();
-                System.Console.WriteLine("***** Red player wins game! *****");
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("\n***** Red player wins game! *****");
+                Console.ForegroundColor = ConsoleColor.White;
                 this.redPlayer.roundsWon = 0;
                 this.DealNewGame();
             }
@@ -110,7 +140,9 @@ namespace Hackathon_Day
                 this.bluePlayer.gamesWon += 1;
                 this.hold = 0;
                 this.DisplayGameStats();
-                System.Console.WriteLine("***** Blue player wins game! *****");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("\n***** Blue player wins game! *****");
+                Console.ForegroundColor = ConsoleColor.White;
                 this.bluePlayer.roundsWon = 0;
                 this.DealNewGame();
             }
@@ -129,21 +161,30 @@ namespace Hackathon_Day
         {
             if(result == 0)
             {
-                System.Console.WriteLine("This round is on hold!");
+                Console.Clear();
+                Console.WriteLine("This round is on hold!");
                 this.hold += 1;
-                System.Console.WriteLine("Rounds on hold: " + this.hold);
+                Console.WriteLine("Rounds on hold: " + this.hold);
+
+                Console.Write("\nPress Any Key to continue: " );
+                Console.ReadLine();
                 this.PlayRound();
             }
             else if(result == 1)
             {
-                System.Console.WriteLine("Red Player Wins round!");
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nRed Player Wins round!");
+                Console.ForegroundColor = ConsoleColor.White;
                 this.redPlayer.roundsWon += 1 + this.hold;
                 this.hold = 0;
                 this.CheckGameOver(this.redPlayer,result);
             }
             else if(result == 2)
             {
-                System.Console.WriteLine("Blue Player Wins round!");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("\nBlue Player Wins round!");
+                Console.ForegroundColor = ConsoleColor.White;
                 this.bluePlayer.roundsWon += 1 + this.hold;
                 this.hold = 0;
                 this.CheckGameOver(this.bluePlayer,result);
